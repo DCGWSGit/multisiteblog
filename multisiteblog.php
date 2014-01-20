3d3af4136ac8d883e74170bd2a54cbde38a90442
Godofredo Padagas Jr <fred.padagas@dcgws.com>
2014-01-20 11:59:47 +0800

    added activation hook

--- multisiteblog.php
+++ multisiteblog.php
@@ -6,3 +6,23 @@ Description: For adding blog from non-wordpress site.
 Version: 1.0 beta
 Author: The DCGWS Team
 */
+
+function activate(){
+	// DB setup here
+	
+}
+
+register_activation_hook( __FILE__, 'activate' );
+
+function deactivate(){
+	//DB Removal here
+}
+
+register_deactivation_hook( __FILE__, 'deactivate' );
+
+
+
+
+Class remotePost{
+
+}
